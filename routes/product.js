746e2b9e7e4a59e0f48095ffaa9300a0d905554d
jirasak <jirasakwaennak1776@gmail.com>
2024-07-23 01:10:24 +0700

    vslab5

---
+++ routes/product.js
@@ -0,0 +1,10 @@
+const express = require('express'); 
+const { getProducts, getProduct, createProduct, updateProduct, deleteProduct }
+= require('../controllers/productController'); 
+const router = express.Router();
+router.get('/products', getProducts);
+router.get('/product/:id', getProduct);
+router.post('/product', createProduct);
+router.put('/product/:id', updateProduct);
+router.delete('/product/:id',  deleteProduct);
+module.exports = router;
